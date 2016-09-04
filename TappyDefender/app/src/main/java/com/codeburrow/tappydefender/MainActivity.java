2a76bc0d6153385be1097d3f596c0a3506f8486f
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-04 19:47:49 +0300

    Add functionality to the MainActivity.

--- TappyDefender/app/src/main/java/com/codeburrow/tappydefender/MainActivity.java
+++ TappyDefender/app/src/main/java/com/codeburrow/tappydefender/MainActivity.java
@@ -1,13 +1,29 @@
 package com.codeburrow.tappydefender;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        // Listen for clicks.
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        // Start the GameActivity class via the Intent.
+        startActivity(intent);
+        // And shut this activity down.
+        finish();
     }
 }
