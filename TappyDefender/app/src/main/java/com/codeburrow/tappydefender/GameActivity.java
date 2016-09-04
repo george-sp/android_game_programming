a61da64e3416807772040e56d0543dccfbbc5824
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-04 21:08:19 +0300

    Define the outline of the code in GameActivity.

--- TappyDefender/app/src/main/java/com/codeburrow/tappydefender/GameActivity.java
+++ TappyDefender/app/src/main/java/com/codeburrow/tappydefender/GameActivity.java
@@ -5,9 +5,30 @@ import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
 
+    // The object to handle the game view.
+    private TappyDefenderView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+
+        // Create an instance of our TappyDefenderView.
+        gameView = new TappyDefenderView(this);
+        // Make our "gameView" the view for the Activity.
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // If the Activity is paused make sure to pause our thread.
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // If the Activity is resumed make sure to resume our thread.
+        gameView.resume();
     }
 }
