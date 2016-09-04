eec0fb4944fe0000627b87ce1a19fdab46c4602b
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-04 20:17:27 +0300

    Create the TappyDefenderView, the view of the game.

---
+++ TappyDefender/app/src/main/java/com/codeburrow/tappydefender/TappyDefenderView.java
@@ -0,0 +1,19 @@
+package com.codeburrow.tappydefender;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * This class represents the view of our game. -- A dynamically drawn view.
+ */
+public class TappyDefenderView extends SurfaceView implements Runnable {
+
+    public TappyDefenderView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
