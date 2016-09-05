9e1599b2d64e05361d0c2d06505404c24085acb1
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-05 21:50:30 +0300

    Create the EnemyShip object and spawn the enemy.

---
+++ TappyDefender/app/src/main/java/com/codeburrow/tappydefender/EnemyShip.java
@@ -0,0 +1,53 @@
+package com.codeburrow.tappydefender;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.Random;
+
+/**
+ * A class for the enemy's spaceship.
+ */
+public class EnemyShip {
+
+    private Bitmap bitmap;
+    private int x, y;
+    private int speed = 1;
+
+    // Detect enemies leaving the screen.
+    private int maxX;
+    private int minX;
+
+    // Spawn enemies within screen bounds.
+    private int maxY;
+    private int minY;
+
+    // Constructor
+    public EnemyShip(Context context, int screenX, int screenY) {
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy);
+
+        maxX = screenX;
+        maxY = screenY;
+        minX = 0;
+        minY = 0;
+
+        Random generator = new Random();
+        speed = generator.nextInt(6) + 10;
+
+        x = screenX;
+        y = generator.nextInt(maxY) - bitmap.getHeight();
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
