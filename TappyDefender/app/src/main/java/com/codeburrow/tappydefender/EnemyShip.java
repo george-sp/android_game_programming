149817cc8fc98192a951120f18c948504b7f7ec8
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-07 10:22:24 +0300

    Use multiple different enemy graphics.

--- TappyDefender/app/src/main/java/com/codeburrow/tappydefender/EnemyShip.java
+++ TappyDefender/app/src/main/java/com/codeburrow/tappydefender/EnemyShip.java
@@ -29,14 +29,25 @@ public class EnemyShip {
 
     // Constructor
     public EnemyShip(Context context, int screenX, int screenY) {
-        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy);
+        Random random = new Random();
+        int whichBitmap = random.nextInt(3);
+        switch (whichBitmap) {
+            case 0:
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy3);
+                break;
+            case 1:
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy2);
+                break;
+            case 2:
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy);
+                break;
+        }
 
         maxX = screenX;
         maxY = screenY;
         minX = 0;
         minY = 0;
 
-        Random random = new Random();
         speed = random.nextInt(6) + 10;
 
         x = screenX;
