5dd45f8b3d30dd53a4d6e40d84a75ceefc89d55d
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-10 23:56:00 +0300

    Configure the player buttons.

--- Platformer/app/src/main/java/com/codeburrow/platformer/InputController.java
+++ Platformer/app/src/main/java/com/codeburrow/platformer/InputController.java
@@ -1,12 +1,62 @@
 package com.codeburrow.platformer;
 
+import android.graphics.Rect;
+
+import java.util.ArrayList;
+
 /**
  * Detects the player's input.
  */
 public class InputController {
 
-    public InputController(int screenWidth, int screenHeight) {
+    Rect left;
+    Rect right;
+    Rect jump;
+    Rect shoot;
+    Rect pause;
 
+    InputController(int screenWidth, int screenHeight) {
+
+        // Configure the player buttons.
+        int buttonWidth = screenWidth / 8;
+        int buttonHeight = screenHeight / 7;
+        int buttonPadding = screenWidth / 80;
+
+        left = new Rect(buttonPadding,
+                screenHeight - buttonHeight - buttonPadding,
+                buttonWidth,
+                screenHeight - buttonPadding);
+
+        right = new Rect(buttonWidth + buttonPadding,
+                screenHeight - buttonHeight - buttonPadding,
+                buttonWidth + buttonPadding + buttonWidth,
+                screenHeight - buttonPadding);
+
+        jump = new Rect(screenWidth - buttonWidth - buttonPadding,
+                screenHeight - buttonHeight - buttonPadding - buttonHeight - buttonPadding,
+                screenWidth - buttonPadding,
+                screenHeight - buttonPadding - buttonHeight - buttonPadding);
+
+        shoot = new Rect(screenWidth - buttonWidth - buttonPadding,
+                screenHeight - buttonHeight - buttonPadding,
+                screenWidth - buttonPadding,
+                screenHeight - buttonPadding);
+
+        pause = new Rect(screenWidth - buttonPadding - buttonWidth,
+                buttonPadding,
+                screenWidth - buttonPadding,
+                buttonPadding + buttonHeight);
+    }
+
+    public ArrayList getButtons() {
+        // Create an array of buttons for the draw method.
+        ArrayList<Rect> currentButtonList = new ArrayList<>();
+        currentButtonList.add(left);
+        currentButtonList.add(right);
+        currentButtonList.add(jump);
+        currentButtonList.add(shoot);
+        currentButtonList.add(pause);
+        return currentButtonList;
     }
 
 }
