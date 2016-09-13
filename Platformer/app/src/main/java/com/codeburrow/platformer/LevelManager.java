306dccba4e6130e48e71df4e6a5422bed21b3fff
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-13 21:16:27 +0300

    Apply minor updates to LevelManager class, so to expect the new objects.

--- Platformer/app/src/main/java/com/codeburrow/platformer/LevelManager.java
+++ Platformer/app/src/main/java/com/codeburrow/platformer/LevelManager.java
@@ -84,6 +84,15 @@ public class LevelManager {
             case 'p':
                 index = 2;
                 break;
+            case 'c':
+                index = 3;
+                break;
+            case 'u':
+                index = 4;
+                break;
+            case 'e':
+                index = 5;
+                break;
             default:
                 index = 0;
                 break;
@@ -111,6 +120,15 @@ public class LevelManager {
                 break;
             case 'p':
                 index = 2;
+                break;
+            case 'c':
+                index = 3;
+                break;
+            case 'u':
+                index = 4;
+                break;
+            case 'e':
+                index = 5;
                 break;
             default:
                 index = 0;
@@ -158,11 +176,23 @@ public class LevelManager {
                             player = (Player) gameObjects.get(playerIndex);
 
                             break;
+                        case 'c':
+                            // Add a coin to the gameObjects.
+                            gameObjects.add(new Coin(j, i, c));
+                            break;
+                        case 'u':
+                            // Add a machine gun upgrade to the gameObjects.
+                            gameObjects.add(new MachineGunUpgrade(j, i, c));
+                            break;
+                        case 'e':
+                            // Add an extra life to the gameObjects.
+                            gameObjects.add(new ExtraLife(j, i, c));
+                            break;
                     }
+                }
 
-                    if (bitmapsArray[getBitmapIndex(c)] == null) {
-                        bitmapsArray[getBitmapIndex(c)] = gameObjects.get(currentIndex).prepareBitmap(context, gameObjects.get(currentIndex).getBitmapName(), pixelsPerMetre);
-                    }
+                if (bitmapsArray[getBitmapIndex(c)] == null) {
+                    bitmapsArray[getBitmapIndex(c)] = gameObjects.get(currentIndex).prepareBitmap(context, gameObjects.get(currentIndex).getBitmapName(), pixelsPerMetre);
                 }
             }
         }
