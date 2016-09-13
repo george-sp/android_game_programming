987c1e02a50b60af0927ff253575353d60926e35
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-13 21:26:51 +0300

    Add a handy list of all game object types.

--- Platformer/app/src/main/java/com/codeburrow/platformer/LevelData.java
+++ Platformer/app/src/main/java/com/codeburrow/platformer/LevelData.java
@@ -16,5 +16,33 @@ public class LevelData {
      * ------------------------
      * . = no tile
      * 1 = Grass
+     * 2 = Snow
+     * 3 = Brick
+     * 4 = Coal
+     * 5 = Concrete
+     * 6 = Scorched
+     * 7 = Stone
+     */
+
+    /*
+     * Active objects
+     * g = guard
+     * d = drone
+     * t = teleport
+     * c = coin
+     * u = upgrade
+     * f = fire
+     * e  = extra life
+     */
+
+    /*
+     * Inactive objects
+     * w = tree
+     * x = tree2 (snowy)
+     * l = lampost
+     * r = stalactite
+     * s = stalacmite
+     * m = mine cart
+     * z = boulders
      */
 }
