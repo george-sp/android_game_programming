8b6fb7efe9706b05cbe08e2972368f2ff5a37ca2
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-17 00:07:18 +0300

    Change the center of focus of Viewport.

--- Platformer/app/src/main/java/com/codeburrow/platformer/game_engine/Viewport.java
+++ Platformer/app/src/main/java/com/codeburrow/platformer/game_engine/Viewport.java
@@ -41,10 +41,35 @@ public class Viewport {
         currentViewportWorldCentre = new Vector2Point();
     }
 
+    public void moveViewportRight(int maxWidth) {
+        if (currentViewportWorldCentre.x < maxWidth -
+                (metresToShowX / 2) + 3) {
+            currentViewportWorldCentre.x += 1;
+        }
+    }
+
+    public void moveViewportLeft() {
+        if (currentViewportWorldCentre.x > (metresToShowX / 2) - 3) {
+            currentViewportWorldCentre.x -= 1;
+        }
+    }
+
+    public void moveViewportUp() {
+        if (currentViewportWorldCentre.y > (metresToShowY / 2) - 3) {
+            currentViewportWorldCentre.y -= 1;
+        }
+    }
+
+    public void moveViewportDown(int maxHeight) {
+        if (currentViewportWorldCentre.y <
+                maxHeight - (metresToShowY / 2) + 3) {
+            currentViewportWorldCentre.y += 1;
+        }
+    }
+
     void setWorldCentre(float x, float y) {
         currentViewportWorldCentre.x = x;
         currentViewportWorldCentre.y = y;
-
     }
 
     public int getScreenWidth() {
