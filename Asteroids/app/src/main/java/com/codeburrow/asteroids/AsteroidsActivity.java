ea45eb43650cef89a6e787260e47f34f70783d19
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-17 23:24:17 +0300

    Lock the game in landscape mode.

--- Asteroids/app/src/main/java/com/codeburrow/asteroids/AsteroidsActivity.java
+++ Asteroids/app/src/main/java/com/codeburrow/asteroids/AsteroidsActivity.java
@@ -3,11 +3,10 @@ package com.codeburrow.asteroids;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AsteroidsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
