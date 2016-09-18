710e387135b73c393259ad27066a48de01b2ab0c
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-18 22:33:39 +0300

    Create the AsteroidsRenderer class.

---
+++ Asteroids/app/src/main/java/com/codeburrow/asteroids/AsteroidsRenderer.java
@@ -0,0 +1,44 @@
+package com.codeburrow.asteroids;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ *
+ */
+public class AsteroidsRenderer implements Renderer {
+
+    public AsteroidsRenderer() {
+        
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+
+    }
+
+    private void createObjects() {
+
+    }
+
+    private void update(long fps) {
+
+    }
+
+
+    private void draw() {
+
+    }
+}
