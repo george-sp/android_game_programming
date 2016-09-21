8cb73ab866eef02f15e0340f25c6131ce2afe3ba
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-21 17:20:32 +0300

    Shoot a bullet by setting facingAngle and speed.

--- Asteroids/app/src/main/java/com/codeburrow/asteroids/Bullet.java
+++ Asteroids/app/src/main/java/com/codeburrow/asteroids/Bullet.java
@@ -1,5 +1,7 @@
 package com.codeburrow.asteroids;
 
+import android.graphics.PointF;
+
 /**
  *
  */
@@ -20,4 +22,35 @@ public class Bullet extends GameObject {
         setVertices(bulletVertices);
     }
 
+    /**
+     * Sets the facingAngle of the bullet to that of the ship.
+     * This will cause the bullet to move in the direction the ship was facing
+     * at the time the fire button was pressed.
+     *
+     * @param shipFacingAngle
+     */
+    public void shoot(float shipFacingAngle) {
+        setFacingAngle(shipFacingAngle);
+        inFlight = true;
+        setSpeed(300);
+    }
+
+    /**
+     * Sets the bullet inside the ship and cancels its velocity and speed.
+     * The bullet will sit invisibly inside the ship until it is fired.
+     *
+     * @param shipLocation
+     */
+    public void resetBullet(PointF shipLocation) {
+        // Remove the velocity if bullet out of bounds.
+        inFlight = false;
+        setxVelocity(0);
+        setyVelocity(0);
+        setSpeed(0);
+        setWorldLocation(shipLocation.x, shipLocation.y);
+    }
+
+    public boolean isInFlight() {
+        return inFlight;
+    }
 }
