b8456406beb204cd67cccac828d19ae75d3b81e9
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-24 23:46:52 +0300

    Add a collision package to the SpaceShip class.

--- Asteroids/app/src/main/java/com/codeburrow/asteroids/SpaceShip.java
+++ Asteroids/app/src/main/java/com/codeburrow/asteroids/SpaceShip.java
@@ -1,5 +1,7 @@
 package com.codeburrow.asteroids;
 
+import android.graphics.PointF;
+
 /**
  * Space ship is the player's game object.
  */
@@ -8,6 +10,12 @@ public class SpaceShip extends GameObject {
     boolean isThrusting;
     private boolean isPressingRight = false;
     private boolean isPressingLeft = false;
+
+    CollisionPackage collisionPackage;
+
+    // Next, a 2d representation using PointF of the vertices.
+    // Used to build asteroidVertices and to pass to the CollisionPackage constructor.
+    PointF[] points;
 
     public SpaceShip(float worldLocationX, float worldLocationY) {
         super();
@@ -37,6 +45,32 @@ public class SpaceShip extends GameObject {
                 0, 0 + halfL, 0
         };
         setVertices(shipVertices);
+
+        points = new PointF[6];
+        points[0] = new PointF(-halfW, -halfL);
+
+        points[2] = new PointF(halfW, -halfL);
+        points[4] = new PointF(0, 0 + halfL);
+
+        /*
+         * To make collision detection more accurate we will define some
+         * more points on the midpoints of all our sides.
+         * It is possible that the point of an asteroid will pass through
+         * the side of the ship and we do not test for this!
+         * We only test for the point of a ship passing through the side of an asteroid!!
+         * This is computationally cheaper than running both tests.
+         * Although not as accurate we will see it is very close.
+         * We can think of this visually as adding extra sensors on the sides of our ship
+         * Here we use an equation to find the midpoint of a line which you can find
+         * on most good high school math web sites.
+         */
+        points[1] = new PointF(points[0].x + points[2].x / 2, (points[0].y + points[2].y) / 2);
+        points[3] = new PointF((points[2].x + points[4].x) / 2, (points[2].y + points[4].y) / 2);
+        points[5] = new PointF((points[4].x + points[0].x) / 2, (points[4].y + points[0].y) / 2);
+
+        // Initialize the collision package.
+        // (the object space vertex list, x any world location the largest possible radius, facingAngle)
+        collisionPackage = new CollisionPackage(points, getWorldLocation(), length / 2, getFacingAngle());
     }
 
     public void setPressingRight(boolean pressingRight) {
@@ -86,5 +120,9 @@ public class SpaceShip extends GameObject {
         }
 
         move(fps);
+
+        // Update the collision package.
+        collisionPackage.facingAngle = getFacingAngle();
+        collisionPackage.worldLocation = getWorldLocation();
     }
 }
