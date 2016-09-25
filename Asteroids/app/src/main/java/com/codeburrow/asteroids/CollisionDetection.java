213ab9bd52b4db80419a648d70517321d5687b0c
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-25 12:47:39 +0300

    Create the CollisionDetection class.

---
+++ Asteroids/app/src/main/java/com/codeburrow/asteroids/CollisionDetection.java
@@ -0,0 +1,13 @@
+package com.codeburrow.asteroids;
+
+import android.graphics.PointF;
+
+/**
+ * Collision Detection:
+ * The algorithms we will use are computationally expensive,
+ * and we will only want to use them when there is a realistic chance of a collision.
+ */
+public class CollisionDetection {
+
+    private static PointF rotatedPoint = new PointF();
+}
